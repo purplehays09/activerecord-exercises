09d0aff0e85023a1a8f70fb710827a51ad97eec3
purplehays09 <purplehays527@gmail.com>
2021-09-07 10:18:09 -0600

    finished the blog_post test. on to the next

--- blog_posts/spec/models/post_spec.rb
+++ blog_posts/spec/models/post_spec.rb
@@ -25,18 +25,41 @@ require 'rails_helper'
 # A test class for https://github.com/rabid/activerecord-exercises/tree/master/blog_posts.
 RSpec.describe Post do
   subject do
-    Post.new(
+    {
       title: "Title",
       content: "Content"
-    )
+    }
   end
 
   it "list posts" do
     Post.destroy_all
-    subject.save
+    Post.create(subject)
     expect(Post.count).to eq(1) 
   end
   
+  it "creates a post on the backend" do
+    Post.create({title:"test",content:"content"}) 
+    record = Post.find_by(title:'test')
+    expect(record.content).to eq('content')
+  end
+  
+  it "updates a post" do
+    Post.destroy_all
+    Post.create({title:"test",content:"content"}) 
+    record = Post.find_by(title:'test')
+    record.update(content:'new content')
+    post = Post.find_by(title:'test')
+    
+    expect(post.content).to eq('new content') 
+    
+  end
+  
+  it "deletes a post" do
+    Post.destroy_all
+    post = Post.create({title:"test",content:"content"}) 
+    post.destroy
+    expect(Post.count).to eq(0) 
+  end
   
 end
 
