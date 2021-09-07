ce16dad03aa2a4bb972b8db63a087b0e7b937227
purplehays09 <purplehays527@gmail.com>
2021-09-07 13:00:14 -0600

    Added category_spec and initial red test

---
+++ spec/models/category_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe Category do
+  let(:post) do
+    {
+      title:'title',
+      content:'content',
+    }
+  end
+  let(:category) do
+    {
+      name:'name',
+
+    }
+  end
+  it "creates a post with a category" do
+    post.category = Category.create!(category)
+    Post.create!(post)
+    expect(category.name).to eq(Post.category.name) 
+  end
+  
+end
